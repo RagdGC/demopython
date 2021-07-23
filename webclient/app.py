bb7bbc48b301ef280a11aac3378c35d4fc14cdec
Edgar G <edggc@outlook.com>
2021-07-22 23:34:42 -0700

    agregar bootstrap

--- webclient/app.py
+++ webclient/app.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def getIndex():
+def index():
     return render_template("index.html")
 
+@app.route('/empleados')
+def listaempleados():
+    return render_template("empleados/lista.html")
+
+@app.route('/empleados/nuevo')
+def nuevoempleado():
+    return render_template("empleados/nuevo.html")
